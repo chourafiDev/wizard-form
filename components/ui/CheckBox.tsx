069b16a647b3f9169c89e75890033e7e89b7e83d
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-15 13:09:37 +0100

    refactor: change id

--- components/ui/CheckBox.tsx
+++ components/ui/CheckBox.tsx
@@ -14,12 +14,12 @@ const CheckBox = ({ name, value, label, register }: CheckBoxPops) => {
         className="w-4 h-4 accent-brand/40"
         type="checkbox"
         value={value}
-        id={name}
+        id={value}
         {...register(name)}
       />
       <label
         className="text-dark text-sm group-hover:text-brand cursor-pointer duration-200 ease-linear"
-        htmlFor={name}
+        htmlFor={value}
       >
         {label}
       </label>
